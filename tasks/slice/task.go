4c435ce005e1a5671c86307aa1d6e9ab11497e95
Yaoguao <glenioJ@yandex.ru>
2025-07-19 01:32:10 +0400

    add func unique

--- tasks/slice/task.go
+++ tasks/slice/task.go
@@ -11,3 +11,37 @@ func RemoveAtIndex(s []int, index int) []int {
 	}
 	return res
 }
+
+// V1
+func Unique(s []string) []string {
+	res := make([]string, 0, len(s))
+	ch := make(map[string]string)
+	for _, val := range s {
+		if _, ok := ch[val]; ok {
+			continue
+		}
+		ch[val] = val
+		res = append(res, val)
+	}
+	return res
+}
+
+// V2 O(n^2) :D
+//func Unique(s []string) []string {
+//	res := make([]string, 0, len(s))
+//	isUniq := false
+//	for i := 0; i < len(s); i++ {
+//		for j := i + 1; j < len(s); j++ {
+//			if s[i] == s[j] {
+//				isUniq = true
+//				break
+//			}
+//		}
+//		if isUniq {
+//			isUniq = false
+//			continue
+//		}
+//		res = append(res, s[i])
+//	}
+//	return res
+//}
