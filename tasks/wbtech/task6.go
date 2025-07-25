c108f57fe17e0418a207a30c63a71f4928a9df62
Yaoguao <glenioJ@yandex.ru>
2025-07-25 13:19:21 +0400

    add task6

---
+++ tasks/wbtech/task6.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//	Задание 6 *
+//	Реализовать все возможные способы остановки выполнения горутины.
+
+// 1 способ использовать канал
+func worker(ch chan bool) {
+	for {
+		select {
+		case <-ch:
+			fmt.Println("Остановлен")
+			return
+		default:
+			fmt.Println("Работаем")
+			time.Sleep(1 * time.Second)
+		}
+
+	}
+}
+
+func main() {
+	ch := make(chan bool)
+
+	go worker(ch)
+
+	time.Sleep(5 * time.Second)
+	ch <- true
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Программа завершена")
+}
